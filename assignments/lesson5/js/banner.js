a7987de9c41d42f2e1b75a8dc222ade4d127a7e2
MTKnight <god18009@byui.edu>
2020-02-10 11:43:53 -0800

    js fix

--- assignments/lesson5/js/banner.js
+++ assignments/lesson5/js/banner.js
@@ -1,9 +1,10 @@
-var today = new Date();
-var day = today.getDay();
-var disp = "none";
 
-if (day == 5) {
-    disp = "block";
+function displayBanner() {
+    var today = new Date();
+    var day = today.getDay();
+    var disp = "none";
+    if (day == 5) {
+        document.getElementById("banner").style.display = disp;
+    }
 }
 
-document.getElementById("notice").style.display = disp;
