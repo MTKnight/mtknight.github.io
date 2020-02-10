919b0b312a49e90fbd53a9d85b31bde6899cea40
MTKnight <god18009@byui.edu>
2020-02-10 12:02:22 -0800

    pls work js

--- assignments/lesson5/js/banner.js
+++ assignments/lesson5/js/banner.js
@@ -1,10 +1,14 @@
-
-function displayBanner() {
-    var today = new Date();
-    var day = today.getDay();
-    var disp = "none";
-    if (day == 5) {
+function showBanner() {
+    var date = new Date();
+    var names = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var day = names[date.getDay()];
+    
+    if(day=="Friday"){
         document.getElementById("banner").style.display = disp;
+        } else {
+        document.getElementById("banner").style.display = "none";
     }
+    console.log(dayname)
 }
+
 
