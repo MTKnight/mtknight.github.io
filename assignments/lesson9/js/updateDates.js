296c6e9f82ba8026da7adcbf4886b33a2285fff9
MTKnight <god18009@byui.edu>
2020-03-11 23:01:42 -0700

    it's still a hot mess; get help

---
+++ assignments/lesson9/js/updateDates.js
@@ -0,0 +1,7 @@
+function updateDates(){
+	document.getElementById("currentYear").innerHTML = new Date().getFullYear();
+	let lastModif = new Date(document.lastModified);
+	let shortDate = lastModif.toLocaleString();
+	
+	document.getElementById("lastModified").innerHTML = shortDate;
+}
