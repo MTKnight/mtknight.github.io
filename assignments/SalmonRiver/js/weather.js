cbd1c1c291887387ffa4d86da235a9a014d408a0
MTKnight <god18009@byui.edu>
2020-03-31 22:12:14 -0700

    Salmon River Start

---
+++ assignments/SalmonRiver/js/weather.js
@@ -0,0 +1,37 @@
+const appid = "d828b9bbd5a4d69655f7f329aa88e0c8";
+var id = "";
+
+switch(document.getElementById('townname').innerHTML) {
+  case "Riggins, Idaho":
+    id = "4092267";
+    break;
+}
+
+const weather = `https://api.openweathermap.org/data/2.5/weather?id=${id}&units=imperial&APPID=${appid}`;
+fetch(weather)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    document.getElementById('currently').textContent = jsObject.weather[0].main;
+    document.getElementById('current').innerHTML = parseFloat(jsObject.main.temp).toFixed(0) + "&#8457;";
+    document.getElementById('high').innerHTML = parseFloat(jsObject.main.temp_max).toFixed(0) + "&#8457;";
+    document.getElementById('low').innerHTML = parseFloat(jsObject.main.temp_min).toFixed(0) + "&#8457;";
+    document.getElementById('humidity').innerHTML = jsObject.main.humidity + "&percnt;";
+    document.getElementById('windSpeed').textContent = parseFloat(jsObject.wind.speed).toFixed(1) + "mph";
+  });
+
+const forecast = `https://api.openweathermap.org/data/2.5/forecast?id=${id}&units=imperial&APPID=${appid}`;
+fetch(forecast)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    var fiveday = jsObject.list.filter(x => x.dt_txt.includes('18:00:00'));
+
+    for (var i=0; i<fiveday.length; i++) {
+      var d = new Date(fiveday[i].dt_txt);
+      var dayName = days[d.getDay()];
+      document.getElementById("day" + i).textContent = dayName;
+      document.getElementById("temp" + i).innerHTML = parseFloat(fiveday[i].main.temp).toFixed(0) + "&#8457;";
+      document.getElementById("icon" + i).setAttribute("src", `https://openweathermap.org/img/w/${fiveday[i].weather[0].icon}.png`);
+      document.getElementById("icon" + i).setAttribute("alt", fiveday[i].weather[0].description);
+    }
+  });
