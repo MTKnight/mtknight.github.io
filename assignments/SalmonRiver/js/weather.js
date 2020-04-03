7faa88c1077b38c855881a47ad4962c24679b27c
MTKnight <god18009@byui.edu>
2020-04-03 01:24:14 -0700

    forecast

--- assignments/SalmonRiver/js/weather.js
+++ assignments/SalmonRiver/js/weather.js
@@ -1,23 +1,5 @@
 const appid = "d828b9bbd5a4d69655f7f329aa88e0c8";
-var id = "";
-
-switch(document.getElementById('townname').innerHTML) {
-  case "Riggins, Idaho":
-    id = "4092267";
-    break;
-}
-
-const weather = `https://api.openweathermap.org/data/2.5/weather?id=${id}&units=imperial&APPID=${appid}`;
-fetch(weather)
-  .then((response) => response.json())
-  .then((jsObject) => {
-    document.getElementById('currently').textContent = jsObject.weather[0].main;
-    document.getElementById('current').innerHTML = parseFloat(jsObject.main.temp).toFixed(0) + "&#8457;";
-    document.getElementById('high').innerHTML = parseFloat(jsObject.main.temp_max).toFixed(0) + "&#8457;";
-    document.getElementById('low').innerHTML = parseFloat(jsObject.main.temp_min).toFixed(0) + "&#8457;";
-    document.getElementById('humidity').innerHTML = jsObject.main.humidity + "&percnt;";
-    document.getElementById('windSpeed').textContent = parseFloat(jsObject.wind.speed).toFixed(1) + "mph";
-  });
+var id = "4092267";
 
 const forecast = `https://api.openweathermap.org/data/2.5/forecast?id=${id}&units=imperial&APPID=${appid}`;
 fetch(forecast)
