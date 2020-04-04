50cce39d80da996b88da892882a651fee48815ac
MTKnight <god18009@byui.edu>
2020-04-03 23:51:24 -0700

    json fix

--- assignments/SalmonRiver/js/guides.js
+++ assignments/SalmonRiver/js/guides.js
@@ -11,34 +11,28 @@ fetch(requestURL)
     guides.forEach(guide => {
       var card = document.createElement('section');
       var name = document.createElement('h6');
-      var cert = document.createElement('h4');
-      var years = document.createElement('p');
+      var certification = document.createElement('h4');
+      var year = document.createElement('p');
       var email = document.createElement('p');
       var bio = document.createElement('p');
       var image = document.createElement('img');
 
       switch (guide.name) {
         case 'Aragorn II Elessar Telcontar':
-        case 'Frodo':
-        case 'Gandalf':
-        case 'Legolas':
-        case 'Gimli':
-        case 'Samwise':
-        case 'Boromir':
-        case 'Meriadoc':
-        case 'Peregrin':
+        case 'Frodo Baggins':
+        case 'Galdalf':
           name.textContent = guide.name;
-          cert.textContent = guide.certification;
-          years.textContent = `Years Experience: ${guide.yearsExperience}`;
+          certification.textContent = guide.certification;
+          year.textContent = `Years of Experience: ${guide.yearsExperience}`;
           email.textContent = `Email: ${guide.email}`;
-          bio.textContent = `Biography Snapshot: ${guide.bio}`;
+          bio.textContent = `Short Background: ${guide.bio}`;
     
           image.setAttribute('src', `images/${guide.photo}`);
-          image.setAttribute('alt', `${guide.name} - ${guide.cert}`);
+          image.setAttribute('alt', `${guide.name} - ${guide.motto}`);
     
           card.appendChild(name);
-          card.appendChild(cert);
-          card.appendChild(years);
+          card.appendChild(certification);
+          card.appendChild(year);
           card.appendChild(email);
           card.appendChild(bio);
           card.appendChild(image);
